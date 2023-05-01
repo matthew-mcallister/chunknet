e8f8ff1a9e7045477fa9c6f05c4578da5fd3b86f
Matthew McAllister <matthew.mcallister.0@gmail.com>
2023-04-30 22:45:58 -0700

    Add num-params command
    I have no idea why torch runs out of memory as the parameters only take
    take 5G... is it gradient history that's taking up over 5GB?

--- bin/train.py
+++ bin/train.py
@@ -30,18 +30,7 @@ def calc_num_embeddings() -> int:
     return max_emb
 
 
-@click.group()
-def cli() -> None:
-    pass
-
-
-@cli.command()
-def num_embeddings() -> None:
-    print(calc_num_embeddings())
-
-
-@cli.command()
-def train_autoencoder() -> None:
+def create_model() -> AutoencoderLightning:
     num_embeddings = calc_num_embeddings()
     logger.info(f'num_embeddings={num_embeddings}')
     # TODO: Load from config
@@ -56,6 +45,34 @@ def train_autoencoder() -> None:
         embedding_loss_k=0.01,
         learning_rate=1e-4,
     )
+    return encoder
+
+
+@click.group()
+def cli() -> None:
+    pass
+
+
+@cli.command()
+def num_embeddings() -> None:
+    print(calc_num_embeddings())
+
+
+@cli.command()
+def num_params() -> None:
+    n = 0
+    size = 0
+    model = create_model()
+    for param in model.parameters():
+        n += param.nelement()
+        size += n * param.element_size()
+    print(f'number of parameters: {n}')
+    print(f'size of parameters (MB): {int(size / 1e6)}')
+
+
+@cli.command()
+def train_autoencoder() -> None:
+    encoder = create_model()
     # TODO: Separate validation data
     datamodule = ChunkDataModule(chunk_loader(), chunk_loader())
     trainer = Trainer()
